c56887ace229696bba87793d5a5ee298fca260a6
dortiz <dortiz@bitworks.com.sv>
2020-02-13 15:34:52 -0600

    Commit inicial, colocar un texto sobre la imagen

---
+++ src/ImageProcessorTest.Consola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ImageMagick;
+
+namespace ImageProcessorTest.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (MagickImage image = new MagickImage("C:\\Users\\Douglas Ortiz\\Desktop\\google-marker-trans.png"))
+            {
+                new Drawables()// Draw text on the image
+                    .FontPointSize(50)
+                    .Font("Arial")
+                    .StrokeColor(new MagickColor("black"))
+                    .FillColor(MagickColors.White)
+                    .TextAlignment(TextAlignment.Center)
+                    .Text(256, 64, "1")
+                    .StrokeColor(new MagickColor(0, Quantum.Max, 0))
+                    .Draw(image);
+
+                image.Write("C:\\Users\\Douglas Ortiz\\Desktop\\google-marker-trans2.png");
+            }
+        }
+    }
+}
